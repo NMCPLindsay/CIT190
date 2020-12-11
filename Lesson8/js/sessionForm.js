addc13ca300a4b7f19d3691277057c6f34164c77
phil Lindsay <philiplindsay25@gmail.com>
2020-12-11 11:49:59 -0500

    lesson8

---
+++ Lesson8/js/sessionForm.js
@@ -0,0 +1,7 @@
+﻿document.getElementById("sub").addEventListener("click",function(){
+	sessionStorage.firstname = document.getElementById("firstName").value;
+	sessionStorage.lastname = document.getElementById("lastName").value;
+	sessionStorage.email = document.getElementById("email").value;
+	sessionStorage.phone = document.getElementById("phone").value;
+	window.location.assign("sessionConfirm.html");
+});
